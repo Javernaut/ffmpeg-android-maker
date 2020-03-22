603d8dc14aeb86981e22760dfe3a0694c9ea354d
Javernaut <javernaut@gmail.com>
2020-03-22 08:27:37 +0200

    Updating libdav1d to 0.6.0 and nasm 2.14.02

--- scripts/libdav1d/download.sh
+++ scripts/libdav1d/download.sh
@@ -4,7 +4,7 @@
 
 # Exports SOURCES_DIR_libdav1d - path where actual sources are stored
 
-DAV1D_VERSION=0.5.2
+DAV1D_VERSION=0.6.0
 echo "Using libdav1d $DAV1D_VERSION"
 DAV1D_SOURCES=dav1d-${DAV1D_VERSION}
 
