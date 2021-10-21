91c15bae02c4bf6384fab5fd62bebec65e11be86
Javernaut <javernaut@gmail.com>
2021-10-21 19:37:49 +0300

    Using Android API 19 as the default one due to upcoming NDK r24 change

--- scripts/parse-arguments.sh
+++ scripts/parse-arguments.sh
@@ -7,7 +7,7 @@
 # Can be overridden with specific arguments.
 # See the end of this file for more description.
 ABIS_TO_BUILD=()
-API_LEVEL=16
+API_LEVEL=19
 SOURCE_TYPE=TAR
 SOURCE_VALUE=4.4
 EXTERNAL_LIBRARIES=()
