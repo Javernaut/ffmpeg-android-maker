9a23b15a1ecdb7c41628a672d7110e499e44c47d
hanbowen <hanbowen@lucigo.com>
2022-06-20 18:01:38 +0800

    fix ci

--- scripts/parse-arguments.sh
+++ scripts/parse-arguments.sh
@@ -116,7 +116,6 @@ for argument in "$@"; do
     ;;
   --enable-mbedtls | -mbedtls)
     EXTERNAL_LIBRARIES+=("mbedtls")
-    FFMPEG_MBEDTLS_ENABLED=true
     ;;
   --enable-libbluray | -bluray)
     EXTERNAL_LIBRARIES+=("libbluray")
