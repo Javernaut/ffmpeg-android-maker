5f813b256dac10016214b9cfa4aa7da29e61115c
Javernaut <javernaut@gmail.com>
2024-04-17 08:37:39 +0200

    Fixing the libx265 compilation, since the source archive structure was changed

--- scripts/libx265/download.sh
+++ scripts/libx265/download.sh
@@ -4,8 +4,6 @@ source ${SCRIPTS_DIR}/common-functions.sh
 
 LIBX265_VERSION=3.6
 
-#  For 3.6 (and presumably above) it is necessary to pass 'true' as the last argument
 downloadTarArchive \
   "libx265" \
-  "https://bitbucket.org/multicoreware/x265_git/downloads/x265_${LIBX265_VERSION}.tar.gz" \
-  true
+  "https://bitbucket.org/multicoreware/x265_git/downloads/x265_${LIBX265_VERSION}.tar.gz"
