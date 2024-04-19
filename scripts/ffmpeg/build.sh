872682baa12b03e7dd8599401a1c4decea49de4e
Javernaut <javernaut@gmail.com>
2024-04-19 23:50:45 +0200

    Checking Vulkan detection

--- scripts/ffmpeg/build.sh
+++ scripts/ffmpeg/build.sh
@@ -43,7 +43,6 @@ DEP_LD_FLAGS="-L${BUILD_DIR_EXTERNAL}/${ANDROID_ABI}/lib $FFMPEG_EXTRA_LD_FLAGS"
   --extra-ldflags="$DEP_LD_FLAGS" \
   --enable-shared \
   --disable-static \
-  --disable-vulkan \
   --pkg-config=${PKG_CONFIG_EXECUTABLE} \
   ${EXTRA_BUILD_CONFIGURATION_FLAGS} \
   $ADDITIONAL_COMPONENTS || exit 1
