e4e2f1a474bb0fd8cfd4c4ec48680625923d08c8
Javernaut <javernaut@gmail.com>
2024-04-20 09:20:46 +0200

    Explicitly enabling Vulkan

--- scripts/ffmpeg/build.sh
+++ scripts/ffmpeg/build.sh
@@ -43,6 +43,7 @@ DEP_LD_FLAGS="-L${BUILD_DIR_EXTERNAL}/${ANDROID_ABI}/lib $FFMPEG_EXTRA_LD_FLAGS"
   --extra-ldflags="$DEP_LD_FLAGS" \
   --enable-shared \
   --disable-static \
+  --enable-vulkan \
   --pkg-config=${PKG_CONFIG_EXECUTABLE} \
   ${EXTRA_BUILD_CONFIGURATION_FLAGS} \
   $ADDITIONAL_COMPONENTS || exit 1
