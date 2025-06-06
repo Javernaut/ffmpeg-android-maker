a9e5426d0b0056dbe4835937f774dbcb8bf41619
Sabina Vasilevskaya <dzucevasabina@gmail.com>
2025-06-06 11:03:18 +0300

    minimal config

--- scripts/ffmpeg/build.sh
+++ scripts/ffmpeg/build.sh
@@ -25,9 +25,18 @@ done
 DEP_CFLAGS="-I${BUILD_DIR_EXTERNAL}/${ANDROID_ABI}/include"
 DEP_LD_FLAGS="-L${BUILD_DIR_EXTERNAL}/${ANDROID_ABI}/lib $FFMPEG_EXTRA_LD_FLAGS"
 
-# Android 15 with 16 kb page size support
-# https://developer.android.com/guide/practices/page-sizes#compile-r27
-EXTRA_LDFLAGS="-Wl,-z,max-page-size=16384 $DEP_LD_FLAGS"
+# Everything that goes below ${EXTRA_BUILD_CONFIGURATION_FLAGS} is my project-specific.
+# You are free to enable/disable whatever you actually need.
+
+SSL_EXTRA_LDFLAGS="-L${SSL_LD}/$ANDROID_ABI/lib"
+SSL_EXTRA_CFLAGS="-I${SSL_LD}/$ANDROID_ABI/include"
+SSL_EXTRA_CONFIG="${SSL_LD}/$ANDROID_ABI/lib/pkgconfig"
+
+echo ${SSL_EXTRA_LDFLAGS}
+echo ${SSL_EXTRA_CFLAGS}
+export PKG_CONFIG_PATH=$SSL_EXTRA_CONFIG
+export PATH=$PATH:$PKG_CONFIG_PATH
+echo $PATH
 
 ./configure \
   --prefix=${BUILD_DIR_FFMPEG}/${ANDROID_ABI} \
@@ -43,13 +52,38 @@ EXTRA_LDFLAGS="-Wl,-z,max-page-size=16384 $DEP_LD_FLAGS"
   --nm=${FAM_NM} \
   --ranlib=${FAM_RANLIB} \
   --strip=${FAM_STRIP} \
-  --extra-cflags="-O3 -fPIC $DEP_CFLAGS" \
-  --extra-ldflags="$EXTRA_LDFLAGS" \
+  --extra-cflags="-O3 -fPIC $DEP_CFLAGS $SSL_EXTRA_CFLAGS" \
+  --extra-ldflags="$DEP_LD_FLAGS $SSL_EXTRA_LDFLAGS -DOPENSSL_API_COMPAT=0x00908000L" \
   --enable-shared \
-  --disable-static \
-  --disable-vulkan \
   --pkg-config=${PKG_CONFIG_EXECUTABLE} \
   ${EXTRA_BUILD_CONFIGURATION_FLAGS} \
+  --enable-small \
+  --disable-ffplay \
+  --disable-ffmpeg \
+  --disable-ffprobe \
+  --disable-avfilter \
+  --disable-avdevice \
+  --disable-doc \
+  --disable-avdevice \
+  --disable-swresample \
+  --disable-postproc \
+  --disable-avfilter \
+  --disable-everything \
+  --enable-decoder=h264 \
+  --disable-gpl \
+  --disable-encoders \
+  --disable-hwaccels \
+  --disable-muxers \
+  --disable-bsfs \
+  --disable-protocols \
+  --disable-indevs \
+  --disable-outdevs \
+  --disable-devices \
+  --disable-filters \
+  --enable-encoder=png \
+  --enable-protocol=file \
+  --disable-debug \
+  --disable-asm \
   $ADDITIONAL_COMPONENTS || exit 1
 
 ${MAKE_EXECUTABLE} clean
