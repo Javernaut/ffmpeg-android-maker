e49a88797f6fb6ee417f51156cb42e9bfdc8b20d
Javernaut <javernaut@gmail.com>
2025-09-20 21:19:30 +0200

    Removing libwavpack support, as it isn't supported by FFmpeg for a long time

--- scripts/libwavpack/download.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-source ${SCRIPTS_DIR}/common-functions.sh
-
-WAVPACK_VERSION=5.7.0
-
-downloadTarArchive \
-  "libwavpack" \
-  "http://www.wavpack.com/wavpack-${WAVPACK_VERSION}.tar.bz2"
